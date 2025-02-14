6518bb1f8996815a0bb11197b23050495ef3f1bc
bernardonogueira8 <62897976+bernardonogueira8@users.noreply.github.com>
2025-02-14 10:46:48 -0300

    up

--- resources/views/layouts/cards.blade.php
+++ resources/views/layouts/cards.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Meu Site' }}</title>
+    <title>{{ $title ?? 'Meus Dashboards' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
